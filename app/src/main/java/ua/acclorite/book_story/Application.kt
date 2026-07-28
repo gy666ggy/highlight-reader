f548ed8938288be407f3c6ead954d3c81129bd37
gy666ggy <1914730752@qq.com>
2026-07-28 08:43:28 +0800

    Register GIF decoder, remove caption, center images (Application.kt)

--- app/src/main/java/ua/acclorite/book_story/Application.kt
+++ app/src/main/java/ua/acclorite/book_story/Application.kt
@@ -7,13 +7,29 @@
 package ua.acclorite.book_story
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
 import dagger.hilt.android.HiltAndroidApp
 import ua.acclorite.book_story.core.crash.CrashHandler
 
 @HiltAndroidApp
-class Application : Application() {
+class Application : Application(), ImageLoaderFactory {
     override fun onCreate() {
         super.onCreate()
         Thread.setDefaultUncaughtExceptionHandler(CrashHandler(this))
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .components {
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+                    add(ImageDecoderDecoder.Factory())
+                } else {
+                    add(GifDecoder.Factory())
+                }
+            }
+            .build()
     }
 }
